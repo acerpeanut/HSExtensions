fceaee5c02599028fa22061eb9982fafab557a9e
acerpeanut <13424344535@163.com>
2019-01-29 15:17:59 +0800

    图片缩放

--- HSExtensions/Category/UIImage+Scale.h
+++ HSExtensions/Category/UIImage+Scale.h
@@ -16,4 +16,29 @@
 
 + (UIImage *)captureView:(UIView *)view;
 
+/**
+ 缩放到指定大小
+
+ @param newSize 新图片的分辨率
+ @return 新图片
+ */
+- (UIImage *)scaleToSize:(CGSize)newSize;
+
+/**
+ 按比例缩放，最大不超过指定大小
+
+ @param maxSize 新图片 宽x高 的最大值
+ @return 新图片
+ */
+- (UIImage *)scaleToMaxSize:(CGFloat)maxSize;
+
+/**
+ 按比例缩放，压缩图片，并转换成NSData
+
+ @param maxSize 新图片 宽x高 的最大值
+ @param compressionQuality 图片压缩质量（0:最差 1:最好）
+ @return 新图片的NSData
+ */
+- (NSData *)compressWithMaxSize:(CGFloat)maxSize compressionQuality:(CGFloat)compressionQuality;
+
 @end
--- HSExtensions/Category/UIImage+Scale.m
+++ HSExtensions/Category/UIImage+Scale.m
@@ -30,4 +30,31 @@
     return graphicsImage;
     
 }
+
+- (UIImage *)scaleToSize:(CGSize)newSize {
+    UIGraphicsBeginImageContext(newSize);
+    [self drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+- (UIImage *)scaleToMaxSize:(CGFloat)maxSize {
+    CGFloat currentSize = self.size.width * self.size.height;
+    if (currentSize > maxSize) {
+        CGFloat delta = sqrt(maxSize/currentSize);
+        CGSize fitSize = CGSizeMake(self.size.width * delta , self.size.height * delta);
+        return [self scaleToSize:fitSize];
+    }
+    return self;
+}
+
+- (NSData *)compressWithMaxSize:(CGFloat)maxSize compressionQuality:(CGFloat)compressionQuality {
+    UIImage *newImage = [self scaleToMaxSize:maxSize];
+    if (newImage == nil) {
+        return nil;
+    }
+    return UIImageJPEGRepresentation(newImage, compressionQuality);
+}
+
 @end
